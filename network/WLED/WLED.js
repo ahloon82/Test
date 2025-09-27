a7219811a1045bdf0db0ec2752dccfbf5e09f1cd
ahloon82 <clarence82ahlun@gmail.com>
2025-09-27 15:22:27 +0800

    Update WLED.js

--- network/WLED/WLED.js
+++ network/WLED/WLED.js
@@ -2399,7 +2399,7 @@ class WLEDDevice {
 						let r = RGBData[led_index * 3];
 						let g = RGBData[led_index * 3 + 1];
 						let b = RGBData[led_index * 3 + 2];
-						let contrast = pickContrastColor([r, g, b]);
+                        let contrast = [255, 255, 255]; // 固定白色
 						RGBData[led_index * 3] = contrast[0];
 						RGBData[led_index * 3 + 1] = contrast[1];
 						RGBData[led_index * 3 + 2] = contrast[2];
